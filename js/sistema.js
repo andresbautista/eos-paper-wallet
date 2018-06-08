673b589908ee20bc0218766671124ee52979ca33
Max G <40041798+maxgonzalees@users.noreply.github.com>
2018-06-08 14:08:21 +0200

    Add files via upload

--- js/sistema.js
+++ js/sistema.js
@@ -1,44 +1,45 @@
 /* Sistema JS per generare eos paper wallet v1 */
-function generate(){
-  var pubkey = document.getElementById("IndirizzoPubblico").value;
-  var privkey = document.getElementById("ChiavePrivata").value;
-  
-  if(pubkey.length<10 || privkey.length<10){
-      alert('Please insert a valid public key.');
-      return;
-  }
-  var pubkey_qr = document.getElementById("IndirizzoPubblico_qr");
-  pubkey_qr.innerHTML="";
-  var privkey_qr = document.getElementById("ChiavePrivata_qr");
-  privkey_qr.innerHTML="";
-  //Genera codici QR
-  new QRCode(pubkey_qr,{
-      text:pubkey,
-      width:100,
-      height:100
-  });
-
-  new QRCode(privkey_qr,{
-      text:privkey,
-      width:128,
-      height:128
-  });
-  //Sostituisci scritte
-  document.getElementById("IndirizzoPubblico").innerHTML = pubkey;
-  document.getElementById("IndirizzoPubblico_testo").innerHTML = pubkey;
-  document.getElementById("ChiavePrivata").innerHTML = privkey;
-  document.getElementById("ChiavePrivata_testo").innerHTML = privkey;
-  $('#checkboxNascondiChiavePrivata').attr('checked', false); 
-  // Nascondi private key
-  $(document).ready(function () {
-        $('#checkboxNascondiChiavePrivata').change(function () {
-            if (!this.checked) {
-            document.getElementById("ChiavePrivata_testo").innerHTML = privkey;
-            }
-            else {
-            document.getElementById("ChiavePrivata_testo").innerHTML = "HIDDEN FOR SECURITY";
-            }
-        });
+var eos = eosjs_ecc
+function inizializza(){
+  // Create a new random private key
+  eos.randomKey().then(privateWif =>  {
+    let publicKey = eos.privateToPublic(privateWif)
+    document.getElementById("publicKey").textContent = publicKey;
+    document.getElementById("privateKey").textContent = privateWif;
+    // Sostituisci testi
+    document.getElementById("IndirizzoPubblico_testo").innerHTML = publicKey;
+    document.getElementById("ChiavePrivata_testo").innerHTML = privateWif;
+    // Genera QR delle chiavi
+    var IndirizzoPubblico_qr = document.getElementById("IndirizzoPubblico_qr");
+    IndirizzoPubblico_qr.innerHTML="";
+    var ChiavePrivata_qr = document.getElementById("ChiavePrivata_qr");
+    ChiavePrivata_qr.innerHTML="";
+    //Genera codici QR
+    new QRCode(IndirizzoPubblico_qr,{
+        text:publicKey,
+        width:100,
+        height:100
     });
+    new QRCode(ChiavePrivata_qr,{
+        text:privateWif,
+        width:100,
+        height:100
+    });
+    $('#checkboxNascondiChiavePrivata').attr('checked', false); 
+    // Nascondi private key
+    $(document).ready(function () {
+          $('#checkboxNascondiChiavePrivata').change(function () {
+              if (!this.checked) {
+              document.getElementById("ChiavePrivata_testo").innerHTML = privateWif;
+              }
+              else {
+              document.getElementById("ChiavePrivata_testo").innerHTML = "HIDDEN FOR SECURITY";
+              }
+          });
+      });
+    //
+  })
 }
+inizializza()
+
 
